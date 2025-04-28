919ba1fb0136ca269c78a5120f47fe9614c435e1
Dashctlab <dashct@gmail.com>
2025-04-28 16:23:49 +0530

    cors url fixed

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://ai-rivu-vercel-frontend.vercel.app'
+}));
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Load users data
