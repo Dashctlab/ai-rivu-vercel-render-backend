196b801ddc8e371ebc4f5ef89431f36fae78a5a6
Dashctlab <dashct@gmail.com>
2025-05-24 13:31:50 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-require('dotenv').config();           // loads from .env
-module.exports = {
-  OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
-  // this is the URL of your staging frontend on Vercel:
-  FRONTEND_URL:        process.env.FRONTEND_URL,
-};
